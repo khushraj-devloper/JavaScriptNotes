39f50de763bb4940ec82d366db20b83e1207787f
Khushraj <khushraj0071@gmail.com>
2026-07-26 11:18:17 +0530

    changes made

---
+++ JsArrayMethods/BasicArrayMethods/basic_methods2.js
@@ -0,0 +1,14 @@
+
+
+let a=[1,2,4,5,5]; 
+
+console.log(a.length) //length of array
+
+
+let s=["html","css","js"]; 
+
+let string=s.toString(); //this method converts the array to string  
+let n=s.join('|'); //returns a new string  html|css|js
+
+console.log(string,typeof string); 
+console.log(n); 
---
+++ JsArrayMethods/BasicArrayMethods/temp.js
@@ -0,0 +1,12 @@
+
+
+let a=["ks","rs",34]
+
+
+let string=a.toString(); 
+
+let m=a.join('/')
+
+console.log(m); 
+console.log(typeof m);
+
