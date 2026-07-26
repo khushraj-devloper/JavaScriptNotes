635eb1b1ec5b23871ef5374e5a20e7f371ec861d
Khushraj <khushraj0071@gmail.com>
2026-07-26 11:14:57 +0530

    changes in notes

--- JsArrayMethods/BasicArrayMethods/basic_methods.js
+++ JsArrayMethods/BasicArrayMethods/basic_methods.js
@@ -11,3 +11,12 @@ const count=numbers.push(1,2,3); // add three elemtns and return the lenght of a
 
 console.log(count); 
 console.log(numbers); 
+
+
+// pop method remove elemetn from last of array and return that element value  
+
+let elemet_removed=numbers.pop(); 
+
+console.log("Element removed is ",elemet_removed); 
+console.log("Updated array  ")
+console.log(numbers); 
