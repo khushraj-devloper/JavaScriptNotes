43ed1d5cf17cbb9899771bbb68e17cc87bf939fd
Khushraj <khushraj0071@gmail.com>
2026-07-26 11:11:56 +0530

    changes in notes

---
+++ JsArrayMethods/BasicArrayMethods/basic_methods.js
@@ -0,0 +1,13 @@
+
+
+
+//some basic methods of array in javascript  
+
+
+const numbers=[1,2,4,4] 
+
+const count=numbers.push(1,2,3); // add three elemtns and return the lenght of array 
+
+
+console.log(count); 
+console.log(numbers); 
